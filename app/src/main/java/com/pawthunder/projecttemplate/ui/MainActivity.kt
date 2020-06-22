a187430ccf757854192f92fa0c55ea86c268faf9
pawthunder <petergrajko@gmail.com>
2020-06-22 23:02:21 +0200

    Add toolbar

--- app/src/main/java/com/pawthunder/projecttemplate/ui/MainActivity.kt
+++ app/src/main/java/com/pawthunder/projecttemplate/ui/MainActivity.kt
@@ -2,10 +2,12 @@ package com.pawthunder.projecttemplate.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.appbar.MaterialToolbar
 import com.pawthunder.projecttemplate.R
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasAndroidInjector
+import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity(), HasAndroidInjector {
@@ -15,6 +17,7 @@ class MainActivity : AppCompatActivity(), HasAndroidInjector {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar as MaterialToolbar)
     }
 
     override fun androidInjector(): AndroidInjector<Any> = dispatchingAndroidInjector

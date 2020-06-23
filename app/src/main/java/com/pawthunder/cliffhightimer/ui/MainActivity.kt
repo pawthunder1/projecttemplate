3e16f100587e514af6468e3b89a99ed50c17af56
pawthunder <petergrajko@gmail.com>
2020-06-23 20:00:52 +0200

    Clean code

--- app/src/main/java/com/pawthunder/cliffhightimer/ui/MainActivity.kt
+++ app/src/main/java/com/pawthunder/cliffhightimer/ui/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.pawthunder.cliffhightimer.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.appbar.MaterialToolbar
 import com.pawthunder.cliffhightimer.R
 import dagger.android.AndroidInjector
